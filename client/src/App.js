787f60f5698378bf80fcc5726ccd7043afb8004c
Balerina <katarzynab9236@gmail.com>
2019-02-08 22:38:10 -0600

    routes work

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Form from './pages/Form';
 import Grid from './pages/Grid';
 import Home from './pages/Home';
 import Portion from './pages/Portion';
 
-class App extends Component {
 
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div>
-        <Form />
-        <Grid />
-        <Home />
-        <Portion />
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/form" component={Form} />
+        <Route exact path="/portion" component={Portion} />
+        <Route exact path="/grid" component={Grid} />
+
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
+
