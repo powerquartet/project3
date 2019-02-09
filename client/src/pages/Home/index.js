0a99e83442f87eee25b9f176a11e2372dbf89029
carnitas <lindsey.cardenas@gmail.com>
2019-02-09 16:53:11 -0600

    home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+import "../Home/css/style.css";
+import logo from "../Home/css/img/plate.jpg";
+
+
 
 class Home extends Component {
+  render() {
+    return (
+      <div>
+        
+        <div className="Home-header">Welcome!</div>
 
-    render() {
-        return (
-            <div>Home</div>
-        )
-    };
+        <div className="Logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="button-group">
+          <a href="/form">
+            <button className="Home-button button1" />
+          </a>
+
+          <a href="/portion">
+            <button className="Home-button button2" />
+          </a>
+
+          <a href="/grid">
+            <button className="Home-button button3" />
+          </a>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
