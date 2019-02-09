472f790f784aebac5bd0fbe1268984df2dac651a
Kinky Cookie <kuki_kukula@yahoo.com>
2019-02-08 22:18:53 -0600

    dnd - 2 working on it

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import HTML5Backend from 'react-dnd-html5-backend';
-// import { DragDropContext } from 'react-dnd';
 import Form from './pages/Form';
 import Grid from './pages/Grid';
 import Home from './pages/Home';
@@ -20,5 +18,4 @@ class App extends Component {
   }
 }
 
-// export default DragDropContext(HTML5Backend)(App)
 export default App;
