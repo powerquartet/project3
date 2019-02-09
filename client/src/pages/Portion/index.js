6e7df4799a8fb392d23d93d1fba0fee1e2d10623
Balerina <katarzynab9236@gmail.com>
2019-02-09 17:03:29 -0600

    buttons added

--- client/src/pages/Portion/index.js
+++ client/src/pages/Portion/index.js
@@ -1,12 +1,59 @@
 import React, { Component } from "react";
+import Toggle from "../../components/Toggle";
+import "./style.css";
+
 
 class Portion extends Component {
-
     render() {
         return (
-            <div>Portion</div>
-        )
-    };
-}
+            <div className="toggleButtons">
 
+                < Toggle>
+                    {({ on, toggle }) => (
+                        <div>
+                            {on && <h1>Fruits</h1>}
+                            <button onClick={toggle}>Fruits</button>
+                        </div>
+                    )}
+                </Toggle>
+
+                < Toggle>
+                    {({ on, toggle }) => (
+                        <div>
+                            {on && <h1>Vegetables</h1>}
+                            <button onClick={toggle}>Vegetables</button>
+                        </div>
+                    )}
+                </Toggle>
+
+                < Toggle>
+                    {({ on, toggle }) => (
+                        <div>
+                            {on && <h1>Priotein</h1>}
+                            <button onClick={toggle}>Protein</button>
+                        </div>
+                    )}
+                </Toggle>
+
+                < Toggle>
+                    {({ on, toggle }) => (
+                        <div>
+                            {on && <h1>Grains</h1>}
+                            <button onClick={toggle}>Grains</button>
+                        </div>
+                    )}
+                </Toggle>
+
+                < Toggle>
+                    {({ on, toggle }) => (
+                        <div>
+                            {on && <h1>Dairy</h1>}
+                            <button onClick={toggle}>Dairy</button>
+                        </div>
+                    )}
+                </Toggle>
+            </div >
+        );
+    }
+}
 export default Portion;
