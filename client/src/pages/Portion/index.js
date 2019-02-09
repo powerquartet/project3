964e7d4196cb339b748bfb07a7fb7825b00b3862
Kinky Cookie <kuki_kukula@yahoo.com>
2019-02-08 20:49:14 -0600

    initial pages set up

---
+++ client/src/pages/Portion/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Portion extends Component {
+
+    render() {
+        return (
+            <div>Portion</div>
+        )
+    };
+}
+
+export default Portion;
