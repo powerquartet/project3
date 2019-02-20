f07b95bf508f3b154c68514e49a15ce02e10a2d9
Balerina <katarzynab9236@gmail.com>
2019-02-19 21:06:24 -0600

    vanilla-flippy-cards

---
+++ client/src/components/CardFlip/images.js
@@ -0,0 +1,19 @@
+import handful from "./images/handful.png"
+import flathand from "./images/flathand.png"
+import onefist from "./images/onefist.png"
+import twofist from "./images/twofist.png"
+import palm from "./images/palm.png"
+import pointerfinger from "./images/pointerfinger.png"
+import thumb from "./images/thumb.png"
+
+const handportions = [
+    { id: 0, src: handful },
+    { id: 1, src: flathand },
+    { id: 2, src: onefist },
+    { id: 3, src: twofist },
+    { id: 4, src: palm },
+    { id: 5, src: pointerfinger },
+    { id: 6, src: thumb }
+]
+
+export default handportions;
