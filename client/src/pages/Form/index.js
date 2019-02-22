cb46c1be8ae3f0e503a6bcbdba2d19a06fbcf366
carnitas <lindsey.cardenas@gmail.com>
2019-02-21 21:26:15 -0600

    landing page things

--- client/src/pages/Form/index.js
+++ client/src/pages/Form/index.js
@@ -1,158 +1,156 @@
 import React, { Component } from "react";
-import API from "../../utils/API"
+import API from "../../utils/API";
+import { NativeTypes } from "react-dnd-html5-backend";
+import Navbar from "../../components/Navbar";
 // import axios from "axios";
 // import calculateBMICalories from '../../components/BMICalculator';
 // import calculateBMI from '../../components/HealthyWeightCalc'
 
-
-
 class Form extends Component {
-    // Setting the component's initial state
+  // Setting the component's initial state
 
-    state = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        weight: 0,
-        height: 0,
-        age: 0,
-        sex: '',
-        userBMI: 0,
-        userBMICalories: 0
-    };
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    weight: 0,
+    height: 0,
+    age: 0,
+    sex: "",
+    userBMI: 0,
+    userBMICalories: 0
+  };
 
+  handleInputChange = event => {
+    console.log(event.target.name);
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
 
+    this.setState({
+      [name]: value
+    });
+  };
 
-    handleInputChange = event => {
-        console.log(event.target.name);
-        // Getting the value and name of the input which triggered the change
-        let value = event.target.value;
-        const name = event.target.name;
+  handleChange = event => {
+    this.setState({ sex: event.target.value });
+  };
 
-        this.setState({
-            [name]: value
-        });
-    };
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    console.log(this.state);
 
-    handleChange = (event) => {
-        this.setState({ sex: event.target.value });
-    };
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    const email = this.state.email;
+    const weight = this.state.weight;
+    const height = this.state.height;
+    const age = this.state.age;
+    const sex = this.state.gender;
 
+    if (this.state.firstName && this.state.email) {
+      API.saveUser({
+        firstName,
+        lastName,
+        email,
+        weight,
+        height,
+        age,
+        sex
+      })
+        .then(res => {
+          console.log(JSON);
+          // this.loadUser();
+          // this.calculatePortionTier();
+        })
+        .catch(err => console.log(err));
 
-    handleFormSubmit = event => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
-        event.preventDefault();
-        console.log(this.state);
-
-        const firstName = this.state.firstName;
-        const lastName = this.state.lastName;
-        const email = this.state.email;
-        const weight = this.state.weight;
-        const height = this.state.height;
-        const age = this.state.age;
-        const sex = this.state.gender;
-
-
-        if (this.state.firstName && this.state.email) {
-            API.saveUser({
-                firstName,
-                lastName,
-                email,
-                weight,
-                height,
-                age,
-                sex
-            })
-                .then(res => {
-                    console.log(JSON);
-                    // this.loadUser();
-                    // this.calculatePortionTier();
-                }).catch(err => console.log(err));
-
-            // userBMICalories = calculateBMI(height, age, gender);
-            // calculateBMICalories(weight, height, age, sex);
-        }
-
-        // let firstName = this.state.firstName;
-        // let lastName = this.state.lastName;
-
-        // if (!this.state.firstName || !this.state.lastName || !this.state.email || !this.state.weight || !this.state.height || !this.state.age) {
-        //     alert("Please fill out all submission fields!");
-        // } else {
-        //     console.log(firstName, lastName);
-
-        // }
-
-    };
-
-    render() {
-        console.log("render method: ", this.state)
-        return (
-            <div>
-                <h1>So, tell us about yourself</h1>
-                <p>
-                    We're here to help you crush it,  {this.state.firstName}
-                </p>
-                <form className="form">
-                    <input
-                        value={this.state.firstName}
-                        name="firstName"
-                        onChange={this.handleInputChange}
-                        type="text"
-                        placeholder="First Name"
-                    />
-                    <input
-                        value={this.state.lastName}
-                        name="lastName"
-                        onChange={this.handleInputChange}
-                        type="text"
-                        placeholder="Last Name"
-                    />
-                    <input
-                        value={this.state.email}
-                        name="email"
-                        onChange={this.handleInputChange}
-                        type="email"
-                        placeholder="Email"
-                    />
-                    <input
-                        value={this.state.weight}
-                        name="weight"
-                        onChange={this.handleInputChange}
-                        type="weight"
-                        placeholder="Weight(lbs)"
-                    />
-                    <input
-                        value={this.state.height}
-                        name="height"
-                        onChange={this.handleInputChange}
-                        type="height"
-                        placeholder="Height(in)"
-                    />
-                    <input
-                        value={this.state.age}
-                        name="age"
-                        onChange={this.handleInputChange}
-                        type="aged"
-                        placeholder="age (years)"
-                    />
-                    <label>
-                        Pick your gender identity:
-                    <select name='gender'
-                            value={this.state.gender}
-                            onChange={this.handleChange}>
-                            <option value="default">choose your sex</option>
-                            <option value="male">male</option>
-                            <option value="female">female</option>
-                            {/* <option value="non-sex-binary">Non-sex binary</option> */}
-                        </select>
-                    </label>
-                    <button className="submit" onClick={this.handleFormSubmit}>Submit</button>
-                </form>
-
-            </div>
-        );
+      // userBMICalories = calculateBMI(height, age, gender);
+      // calculateBMICalories(weight, height, age, sex);
     }
+
+    // let firstName = this.state.firstName;
+    // let lastName = this.state.lastName;
+
+    // if (!this.state.firstName || !this.state.lastName || !this.state.email || !this.state.weight || !this.state.height || !this.state.age) {
+    //     alert("Please fill out all submission fields!");
+    // } else {
+    //     console.log(firstName, lastName);
+
+    // }
+  };
+
+  render() {
+    console.log("render method: ", this.state);
+    return (
+      <div className ="container">
+  
+        <h1>So, tell us about yourself</h1>
+        <p>We're here to help you crush it, {this.state.firstName}</p>
+        <form className="form">
+          <input
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            value={this.state.lastName}
+            name="lastName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          <input
+            value={this.state.email}
+            name="email"
+            onChange={this.handleInputChange}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={this.state.weight}
+            name="weight"
+            onChange={this.handleInputChange}
+            type="weight"
+            placeholder="Weight(lbs)"
+          />
+          <input
+            value={this.state.height}
+            name="height"
+            onChange={this.handleInputChange}
+            type="height"
+            placeholder="Height(in)"
+          />
+          <input
+            value={this.state.age}
+            name="age"
+            onChange={this.handleInputChange}
+            type="aged"
+            placeholder="age (years)"
+          />
+          <label>
+            Pick your gender identity:
+            <select
+              name="gender"
+              value={this.state.gender}
+              onChange={this.handleChange}
+            >
+              <option value="default">choose your sex</option>
+              <option value="male">male</option>
+              <option value="female">female</option>
+              {/* <option value="non-sex-binary">Non-sex binary</option> */}
+            </select>
+          </label>
+          <button className="submit" onClick={this.handleFormSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
--- client/src/pages/Grid/index.js
+++ client/src/pages/Grid/index.js
@@ -2,206 +2,239 @@ import React, { Component } from "react";
 import HTML5Backend from "react-dnd-html5-backend";
 import { DragDropContext } from "react-dnd";
 
-import Item from "../../components/Item"
-import Target from "../../components/Target"
-import Counter from "../../components/Counter"
+import Item from "../../components/Item";
+import Target from "../../components/Target";
+import Counter from "../../components/Counter";
 import plans from "../../plans.json";
+import Navbar from "../../components/Navbar"
 
 //Acess plan from the json tier object
 let plan = plans[4].plan;
 let portions = plans[4].portions;
 
 class Grid extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            plan,
-            portions,
-            newPortions: [],
-            meals: [
-                {
-                    meal: "breakfast",
-                    portions: []
-                }
-                ,
-                {
-                    meal: "snack-1",
-                    portions: []
-                },
-                {
-                    meal: "lunch",
-                    portions: []
-                },
-                {
-                    meal: "snack-2",
-                    portions: []
-                },
-                {
-                    meal: "dinner",
-                    portions: []
-                }]
+  constructor(props) {
+    super(props);
+    this.state = {
+      plan,
+      portions,
+      newPortions: [],
+      meals: [
+        {
+          meal: "breakfast",
+          portions: []
+        },
+        {
+          meal: "snack-1",
+          portions: []
+        },
+        {
+          meal: "lunch",
+          portions: []
+        },
+        {
+          meal: "snack-2",
+          portions: []
+        },
+        {
+          meal: "dinner",
+          portions: []
         }
-    }
-
-    componentDidMount() {
-        let createdPortions = this.state.portions.map((item, index) => {
-            //creating arrays of portions
-            return this.createPortions(item, index)
-            //reducing them to one array
-        }).reduce((allPortions, portionArrays) => {
-            return allPortions.concat(portionArrays)
-        }, []);
-
-        this.setState({
-            newPortions: createdPortions
-        })
-    }
-
-    //create portions based on the number provided by the database
-    createPortions = (portionItem, index) => {
-        const list = [];
-        const fraction = portionItem.portion % 1;
-        let i;
-        // franction is not added with a forloop, therefore needs to start at 1
-        if (fraction === 0) {
-            i = 0
-        } else {
-            i = 1
-        }
-        for (i; i < portionItem.portion; i++) {
-
-            list.push(
-                {
-                    type: portionItem.type,
-                    id: index + "-" + i,
-                    size: 1,
-                    src: portionItem.img
-                }
-            )
-        }
-        //if there's a fraction of the portion remaining, add it as a 0.5 size
-        if (fraction !== 0) {
-            list.push(
-                {
-                    type: portionItem.type,
-                    id: index + "-" + fraction,
-                    size: fraction,
-                    src: portionItem.imgHalf
-                }
-            )
-        }
-        // return an array of objects representing each portion of the particular food group
-        return list;
-    }
-
-    moveItem = (itemId, meal) => {
-        this.decrementPortion(itemId);
-        // console.log(`Meal target: ${meal} for item: ${itemId}`)
-        // console.log(this.state.meals[0].portions);
-        for (let i = 0; i < this.state.meals.length; i++) {
-
-            if (meal === this.state.meals[i].meal) {
-
-                for (let j = 0; j < this.state.newPortions.length; j++) {
-
-                    if (itemId === this.state.newPortions[j].id) {
-
-                        const portion = {
-                            id: this.state.newPortions[j].id,
-                            type: this.state.newPortions[j].type,
-                            size: this.state.newPortions[j].size,
-                            src: this.state.newPortions[j].src,
-                        }
-
-                        const updatedPortions = this.state.newPortions.filter((portion) => {
-                            return portion.id !== itemId
-                        })
-
-                        this.setState({
-                            "meals.portionIds": this.state.meals[i].portions.push(portion),
-                            "newPortions": updatedPortions
-                        })
-
-                    }
-                }
-            }
-        }
+      ]
     };
+  }
 
-    decrementPortion = (index) => {
-        let portionIndex = parseInt(index.charAt(0));
-        let isFraction = index.slice(3);
-        //when and Item gets moved, decrement portion
-        for (let i = 0; i < this.state.portions.length; i++) {
-            if (portionIndex === i) {
+  componentDidMount() {
+    let createdPortions = this.state.portions
+      .map((item, index) => {
+        //creating arrays of portions
+        return this.createPortions(item, index);
+        //reducing them to one array
+      })
+      .reduce((allPortions, portionArrays) => {
+        return allPortions.concat(portionArrays);
+      }, []);
 
-                let stateCopy = Object.assign({}, this.state.portions);
+    this.setState({
+      newPortions: createdPortions
+    });
+  }
 
-                if (isFraction === ".5") {
-                    stateCopy[i].portion -= 0.5;
-                } else {
-                    stateCopy[i].portion -= 1;
-                }
+  //create portions based on the number provided by the database
+  createPortions = (portionItem, index) => {
+    const list = [];
+    const fraction = portionItem.portion % 1;
+    let i;
+    // franction is not added with a forloop, therefore needs to start at 1
+    if (fraction === 0) {
+      i = 0;
+    } else {
+      i = 1;
+    }
+    for (i; i < portionItem.portion; i++) {
+      list.push({
+        type: portionItem.type,
+        id: index + "-" + i,
+        size: 1,
+        src: portionItem.img
+      });
+    }
+    //if there's a fraction of the portion remaining, add it as a 0.5 size
+    if (fraction !== 0) {
+      list.push({
+        type: portionItem.type,
+        id: index + "-" + fraction,
+        size: fraction,
+        src: portionItem.imgHalf
+      });
+    }
+    // return an array of objects representing each portion of the particular food group
+    return list;
+  };
 
-                this.setState(stateCopy);
+  moveItem = (itemId, meal) => {
+    this.decrementPortion(itemId);
+    // console.log(`Meal target: ${meal} for item: ${itemId}`)
+    // console.log(this.state.meals[0].portions);
+    for (let i = 0; i < this.state.meals.length; i++) {
+      if (meal === this.state.meals[i].meal) {
+        for (let j = 0; j < this.state.newPortions.length; j++) {
+          if (itemId === this.state.newPortions[j].id) {
+            const portion = {
+              id: this.state.newPortions[j].id,
+              type: this.state.newPortions[j].type,
+              size: this.state.newPortions[j].size,
+              src: this.state.newPortions[j].src
             };
-        };
+
+            const updatedPortions = this.state.newPortions.filter(portion => {
+              return portion.id !== itemId;
+            });
+
+            this.setState({
+              "meals.portionIds": this.state.meals[i].portions.push(portion),
+              newPortions: updatedPortions
+            });
+          }
+        }
+      }
     }
+  };
 
-    render() {
+  decrementPortion = index => {
+    let portionIndex = parseInt(index.charAt(0));
+    let isFraction = index.slice(3);
+    //when and Item gets moved, decrement portion
+    for (let i = 0; i < this.state.portions.length; i++) {
+      if (portionIndex === i) {
+        let stateCopy = Object.assign({}, this.state.portions);
 
-        console.log(this.state.newPortions);
-        const fruit = this.state.newPortions.map((portion, indx) => {
-            return (portion.type === "fruit" ?
-                <Item src={portion.src} size={portion.size} type={portion.type} id={portion.id} key={portion.id} /> : "")
-        });
+        if (isFraction === ".5") {
+          stateCopy[i].portion -= 0.5;
+        } else {
+          stateCopy[i].portion -= 1;
+        }
 
-        const vegetables = this.state.newPortions.map((portion, indx) => {
-            return (portion.type === "vegetable" ?
-                <Item src={portion.src} size={portion.size} type={portion.type} id={portion.id} key={portion.id} /> : "")
-        });
-        const grains = this.state.newPortions.map((portion, indx) => {
-            return (portion.type === "grains" ?
-                <Item src={portion.src} size={portion.size} type={portion.type} id={portion.id} key={portion.id} /> : "")
-        });
+        this.setState(stateCopy);
+      }
+    }
+  };
 
-        const protein = this.state.newPortions.map((portion, indx) => {
-            return (portion.type === "protein" ?
-                <Item src={portion.src} size={portion.size} type={portion.type} id={portion.id} key={portion.id} /> : "")
-        });
+  render() {
+    console.log(this.state.newPortions);
+    const fruit = this.state.newPortions.map((portion, indx) => {
+      return portion.type === "fruit" ? (
+        <Item
+          src={portion.src}
+          size={portion.size}
+          type={portion.type}
+          id={portion.id}
+          key={portion.id}
+        />
+      ) : (
+        ""
+      );
+    });
 
-        const dairy = this.state.newPortions.map((portion, indx) => {
-            return (portion.type === "dairy" ?
-                <Item src={portion.src} size={portion.size} type={portion.type} id={portion.id} key={portion.id} /> : "")
-        });
+    const vegetables = this.state.newPortions.map((portion, indx) => {
+      return portion.type === "vegetable" ? (
+        <Item
+          src={portion.src}
+          size={portion.size}
+          type={portion.type}
+          id={portion.id}
+          key={portion.id}
+        />
+      ) : (
+        ""
+      );
+    });
+    const grains = this.state.newPortions.map((portion, indx) => {
+      return portion.type === "grains" ? (
+        <Item
+          src={portion.src}
+          size={portion.size}
+          type={portion.type}
+          id={portion.id}
+          key={portion.id}
+        />
+      ) : (
+        ""
+      );
+    });
 
-        const targets = this.state.meals.map((meal, indx) => {
-            return <Target portions={this.state.meals[indx].portions} meal={this.state.meals[indx].meal} key={meal.meal} handleDrop={(item, meal) => this.moveItem(item, meal)} />
-        });
+    const protein = this.state.newPortions.map((portion, indx) => {
+      return portion.type === "protein" ? (
+        <Item
+          src={portion.src}
+          size={portion.size}
+          type={portion.type}
+          id={portion.id}
+          key={portion.id}
+        />
+      ) : (
+        ""
+      );
+    });
 
-        return (
-            <div>
-                <Counter portionDailyTarget={this.state.portions} />
-                <div style={{ "float": "left" }}>
-                    {fruit}
-                </div>
-                <div style={{ "float": "left" }}>
-                    {vegetables}
-                </div>
-                <div style={{ "float": "left" }}>
-                    {grains}
-                </div>
-                <div style={{ "float": "left" }}>
-                    {protein}
-                </div>
-                <div style={{ "float": "left" }}>
-                    {dairy}
-                </div>
-                <br style={{ "clear": "both" }} />
-                {targets}
-            </div >
-        );
-    };
-};
+    const dairy = this.state.newPortions.map((portion, indx) => {
+      return portion.type === "dairy" ? (
+        <Item
+          src={portion.src}
+          size={portion.size}
+          type={portion.type}
+          id={portion.id}
+          key={portion.id}
+        />
+      ) : (
+        ""
+      );
+    });
+
+    const targets = this.state.meals.map((meal, indx) => {
+      return (
+        <Target
+          portions={this.state.meals[indx].portions}
+          meal={this.state.meals[indx].meal}
+          key={meal.meal}
+          handleDrop={(item, meal) => this.moveItem(item, meal)}
+        />
+      );
+    });
+
+    return (
+      <div className = "container">
+        <Counter portionDailyTarget={this.state.portions} />
+        <div style={{ float: "left" }}>{fruit}</div>
+        <div style={{ float: "left" }}>{vegetables}</div>
+        <div style={{ float: "left" }}>{grains}</div>
+        <div style={{ float: "left" }}>{protein}</div>
+        <div style={{ float: "left" }}>{dairy}</div>
+        <br style={{ clear: "both" }} />
+        {targets}
+      </div>
+    );
+  }
+}
 
 export default DragDropContext(HTML5Backend)(Grid);
