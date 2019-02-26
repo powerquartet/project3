b55173c62eaabcc894338dbc48a4e84c8cd26f86
Kinky Cookie <kuki_kukula@yahoo.com>
2019-02-26 15:15:38 -0600

    trying to get the mobile grid working

---
+++ client/build/precache-manifest.2f6ea9a71326335776dfac3003e8addf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb4a872177610277ad95de5a80d022fe",
+    "url": "/static/media/basket.eb4a8721.png"
+  },
+  {
+    "revision": "ee8144c19f1869222098",
+    "url": "/static/css/main.c890ece1.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "13a63fde1283ef02baf5",
+    "url": "/static/js/2.13a63fde.chunk.js"
+  },
+  {
+    "revision": "1f2273a4f1815726a5a7c69b0fa700c4",
+    "url": "/static/media/help.1f2273a4.png"
+  },
+  {
+    "revision": "c516e5cb21f51c36a185c79d70800932",
+    "url": "/static/media/food.c516e5cb.jpg"
+  },
+  {
+    "revision": "d81b3b2fd4be775062abe29680a0ad29",
+    "url": "/static/media/heart-food.d81b3b2f.png"
+  },
+  {
+    "revision": "548c3729edb459767e65948173433407",
+    "url": "/static/media/food.548c3729.png"
+  },
+  {
+    "revision": "ee8144c19f1869222098",
+    "url": "/static/js/main.ee8144c1.chunk.js"
+  },
+  {
+    "revision": "7588b4552671d4032592bdce62952a78",
+    "url": "/static/media/handful.7588b455.png"
+  },
+  {
+    "revision": "218d61894e2ea191dd968d7d8cbdcf3e",
+    "url": "/static/media/flathand.218d6189.png"
+  },
+  {
+    "revision": "f1e74ef62023711ba7861d0470b2cb85",
+    "url": "/static/media/onefist.f1e74ef6.png"
+  },
+  {
+    "revision": "7ef2ec2a914698243e5abd6818c04c62",
+    "url": "/static/media/palm.7ef2ec2a.png"
+  },
+  {
+    "revision": "b5f88fb63d7b2b26971d3a92829ff5ba",
+    "url": "/static/media/pointerfinger.b5f88fb6.png"
+  },
+  {
+    "revision": "42ad9fbe0d304846b32030cb27b25063",
+    "url": "/static/media/thumb.42ad9fbe.png"
+  },
+  {
+    "revision": "c3a4b7cf1c245ab77a6841baed1cd7cd",
+    "url": "/static/media/twofist.c3a4b7cf.png"
+  },
+  {
+    "revision": "6b808c99fba169aaf6181fc3be7ff768",
+    "url": "/index.html"
+  }
+];
