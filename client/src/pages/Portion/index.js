f5a4672592da2b87dd9ad2127f7c4fbeecf1e265
Balerina <katarzynab9236@gmail.com>
2019-02-25 18:50:27 -0600

    toggle buttons added

--- client/src/pages/Portion/index.js
+++ client/src/pages/Portion/index.js
@@ -11,102 +11,131 @@ class Portion extends Component {
   constructor(props) {
     super(props);
     console.log(`here are your props: ${props} \n`);
+
+    this.state = {
+      toggleDisplay: false
+    };
+
+
+    this.toggleDisplay = () => {
+      this.setState({ toggleDisplay: !this.state.toggleDisplay });
+    };
   }
+
 
 
   render() {
     return (
       <div className="container">
-        {/* <Wrapper> */}
-        <Row>
-          <Col size="md-12">
-            {/* <div className="toggleButtons"> */}
 
-            <Toggle>
-              {({ on, toggle }) => (
-                <div className="card-container">
-                  {on && (
-                    <div className="card">
-                      1.5 cups means 1 cup raw, frozen or canned / .5 cups dried
+        {this.state.toggleDisplay ? (
+          <div><Row>
+            <Col size="md-12">
+              {/* <div className="toggleButtons"> */}
+
+              <Toggle>
+                {({ on, toggle }) => (
+                  <div className="card-container">
+                    {on && (
+                      <div className="card">
+                        1.5 cups means 1 cup raw, frozen or canned / .5 cups dried
                     </div>
-                  )}
-                  <button className="food-group" onClick={toggle}>
-                    Fruits
+                    )}
+                    <button className="food-group" onClick={toggle}>
+                      Fruits
                   </button>
-                </div>
-              )}
-            </Toggle>
+                  </div>
+                )}
+              </Toggle>
 
-            <Toggle>
-              {({ on, toggle }) => (
-                <div className="card-container">
-                  {on && (
-                    <div className="card">
-                      2.5 cups means 1 cup raw, frozen or canned / 2 cups leafy
-                      / 1 cup juiced
+              <Toggle>
+                {({ on, toggle }) => (
+                  <div className="card-container">
+                    {on && (
+                      <div className="card">
+                        2.5 cups means 1 cup raw, frozen or canned / 2 cups leafy
+                        / 1 cup juiced
                     </div>
-                  )}
-                  <button className="food-group" onClick={toggle}>
-                    Vegetables
+                    )}
+                    <button className="food-group" onClick={toggle}>
+                      Vegetables
                   </button>
-                </div>
-              )}
-            </Toggle>
+                  </div>
+                )}
+              </Toggle>
 
-            <Toggle>
-              {({ on, toggle }) => (
-                <div className="card-container">
-                  {on && (
-                    <div className="card">
-                      5 oz means 1 oz cooked or canned lean meat like poultry or
-                      seafood / 1 egg / 1 tbsp peanut butter / 1/4 cup cooked
-                      beans or peas / 1/2 oz of nuts or seeds
+              <Toggle>
+                {({ on, toggle }) => (
+                  <div className="card-container">
+                    {on && (
+                      <div className="card">
+                        5 oz means 1 oz cooked or canned lean meat like poultry or
+                        seafood / 1 egg / 1 tbsp peanut butter / 1/4 cup cooked
+                        beans or peas / 1/2 oz of nuts or seeds
                     </div>
-                  )}
-                  <button className="food-group" onClick={toggle}>
-                    Protein
+                    )}
+                    <button className="food-group" onClick={toggle}>
+                      Protein
                   </button>
-                </div>
-              )}
-            </Toggle>
+                  </div>
+                )}
+              </Toggle>
 
-            <Toggle>
-              {({ on, toggle }) => (
-                <div className="card-container">
-                  {on && (
-                    <div className="card">
-                      6 oz means 1 slice of bread / 1 oz of ready to eat cereal
+              <Toggle>
+                {({ on, toggle }) => (
+                  <div className="card-container">
+                    {on && (
+                      <div className="card">
+                        6 oz means 1 slice of bread / 1 oz of ready to eat cereal
                     </div>
-                  )}
-                  <button className="food-group" onClick={toggle}>
-                    Grains
+                    )}
+                    <button className="food-group" onClick={toggle}>
+                      Grains
                   </button>
-                </div>
-              )}
-            </Toggle>
+                  </div>
+                )}
+              </Toggle>
 
-            <Toggle>
-              {({ on, toggle }) => (
-                <div className="card-container">
-                  {on && (
-                    <div className="card">
-                      3 cups means 1 cup of milk / 1 cup of yogurt / 1 oz of fat
-                      cheese / 1.5 oz of natural cheese / 1 cup of procedded
-                      cheese
+              <Toggle>
+                {({ on, toggle }) => (
+                  <div className="card-container">
+                    {on && (
+                      <div className="card">
+                        3 cups means 1 cup of milk / 1 cup of yogurt / 1 oz of fat
+                        cheese / 1.5 oz of natural cheese / 1 cup of procedded
+                        cheese
                     </div>
-                  )}
-                  <button className="food-group" onClick={toggle}>
-                    Dairy
+                    )}
+                    <button className="food-group" onClick={toggle}>
+                      Dairy
                   </button>
-                </div>
-              )}
-            </Toggle>
+                  </div>
+                )}
+              </Toggle>
+            </Col>
+          </Row>
+            < Row >
+              <Col size="md-12">
+                <button onClick={this.toggleDisplay}> How Do I Hand Portion? </button>
+              </Col>
+            </Row>
+          </div>
+        ) : (
+            <div><Row>
+              <Col size="md-12">
 
-            <CardFlip />
-            {/* </div > */}
-          </Col>
-        </Row>
-        {/* </Wrapper> */}
+                <CardFlip />
+              </Col>
+            </Row>
+              < Row >
+                <Col size="md-12">
+                  <button onClick={this.toggleDisplay}> My Daily Portions </button>
+                </Col>
+              </Row>
+            </div>
+
+          )}
+
       </div>
     );
   }
